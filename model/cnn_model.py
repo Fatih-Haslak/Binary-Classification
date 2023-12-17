9edf515c96a2c2888eed398a9fbc30bbcdcdffd4
Fatih HAŞLAK <62775596+Fatih-Haslak@users.noreply.github.com>
2023-12-17 18:17:42 +0300

    vgg16 model with GELU

--- model/cnn_model.py
+++ model/cnn_model.py
@@ -1,112 +1,100 @@
 import torch
 import torch.nn as nn
-import torch.optim as optim
 import torch.nn.functional as F
 
-class ResidualBlock(nn.Module):
-    def __init__(self, in_channels, out_channels, stride=1):
-        super(ResidualBlock, self).__init__()
-        self.conv1 = nn.Conv2d(in_channels, out_channels, kernel_size=3, stride=stride, padding=1)
-        self.batchnorm1 = nn.BatchNorm2d(out_channels)
-        self.relu1 = nn.GELU()
-        
-        self.conv2 = nn.Conv2d(out_channels, out_channels, kernel_size=3, stride=1, padding=1)
-        self.batchnorm2 = nn.BatchNorm2d(out_channels)
-        self.relu2 = nn.GELU()
-
-        # Eğer boyut değişimi varsa, residual bağlantıyı uygun hale getir
-        self.downsample = nn.Sequential(
-            nn.Conv2d(in_channels, out_channels, kernel_size=1, stride=stride),
-            nn.BatchNorm2d(out_channels)
-        ) if stride != 1 or in_channels != out_channels else None
-
-    def forward(self, x):
-        residual = x
-
-        out = self.conv1(x)
-        out = self.batchnorm1(out)
-        out = self.relu1(out)
-
-        out = self.conv2(out)
-        out = self.batchnorm2(out)
-
-        # Residual bağlantıyı ekleyin
-        if self.downsample is not None:
-            residual = self.downsample(x)
-
-        out += residual
-        out = self.relu2(out)
-
-        return out
 
 class CNNModel(nn.Module):
     def __init__(self):
         super(CNNModel, self).__init__()
-        self.conv1 = nn.Conv2d(3, 16, kernel_size=3, stride=1, padding=1)
-        self.batchnorm1 = nn.BatchNorm2d(16)
-        self.relu1 = nn.GELU()
-        self.pool1 = nn.MaxPool2d(kernel_size=2, stride=2)
-        self.conv2 = nn.Conv2d(16, 32, kernel_size=3, stride=1, padding=1)
-        self.batchnorm2 = nn.BatchNorm2d(32)
-        self.resblock1 = ResidualBlock(32, 64)
+
+        self.layer1 = nn.Sequential(
+            nn.Conv2d(3, 64, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(64),
+            nn.GELU())
+        self.layer2 = nn.Sequential(
+            nn.Conv2d(64, 64, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(64),
+            nn.GELU(), 
+            nn.MaxPool2d(kernel_size = 2, stride = 2))
+        self.layer3 = nn.Sequential(
+            nn.Conv2d(64, 128, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(128),
+            nn.GELU())
+        self.layer4 = nn.Sequential(
+            nn.Conv2d(128, 128, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(128),
+            nn.GELU(),
+            nn.MaxPool2d(kernel_size = 2, stride = 2))
+        self.layer5 = nn.Sequential(
+            nn.Conv2d(128, 256, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(256),
+            nn.GELU())
+        self.layer6 = nn.Sequential(
+            nn.Conv2d(256, 256, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(256),
+            nn.GELU())
+        self.layer7 = nn.Sequential(
+            nn.Conv2d(256, 256, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(256),
+            nn.GELU(),
+            nn.MaxPool2d(kernel_size = 2, stride = 2))
+        self.layer8 = nn.Sequential(
+            nn.Conv2d(256, 512, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(512),
+            nn.GELU())
+        self.layer9 = nn.Sequential(
+            nn.Conv2d(512, 512, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(512),
+            nn.GELU())
+        self.layer10 = nn.Sequential(
+            nn.Conv2d(512, 512, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(512),
+            nn.GELU(),
+            nn.MaxPool2d(kernel_size = 2, stride = 2))
+        self.layer11 = nn.Sequential(
+            nn.Conv2d(512, 512, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(512),
+            nn.GELU())
+        self.layer12 = nn.Sequential(
+            nn.Conv2d(512, 512, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(512),
+            nn.GELU())
+        self.layer13 = nn.Sequential(
+            nn.Conv2d(512, 512, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(512),
+            nn.GELU(),
+            nn.MaxPool2d(kernel_size = 2, stride = 2))
+        self.fc = nn.Sequential(
+            nn.Dropout(0.5),
+            nn.Linear(7*7*512, 4096),
+            nn.GELU())
+        self.fc1 = nn.Sequential(
+            nn.Dropout(0.5),
+            nn.Linear(4096, 4096),
+            nn.ReLU())
+        self.fc2= nn.Sequential(
+            nn.Linear(4096, 1),
+            nn.Sigmoid())
         
-        self.resblock2 = ResidualBlock(64, 128)
-        
-        self.resblock3 = ResidualBlock(128, 128)
-        
-        self.fc1 = nn.Linear(131072, 256)
-        self.batchnorm4 = nn.LayerNorm(256)
-        self.batchnorm5 = nn.LayerNorm(128)
-        self.batchnorm6 = nn.LayerNorm(64)
-        self.relu4 = nn.GELU()
-        self.fc1_1 = nn.Linear(256, 128)
-        self.fc2 = nn.Linear(128, 64)
-        self.fc3 = nn.Linear(64, 32)
-        self.fc4 = nn.Linear(32, 16)
-        self.fc5 = nn.Linear(16, 1)
-        self.sigmoid = nn.Sigmoid()
 
     def forward(self, x):
-        x = self.conv1(x)
-        x = self.batchnorm1(x)
-        x = self.relu1(x)
-        x = self.pool1(x)
-        
-        x = self.conv2(x)
-        x = self.batchnorm2(x)
-        x = self.relu1(x)
-      
-        x = self.pool1(x)
-        x = self.resblock1(x)
-        x = self.resblock2(x)
-        x = self.resblock3(x)
-        # print(x.size())
-        x = x.view(x.size(0),-1) #önemli nokta.
-        # print(x.size())
-        x = self.fc1(x)
-        #=nn.Linear(131072, 256)
-        x = self.batchnorm4(x)
-      
-        x = self.relu4(x)
-        
-        x = self.fc1_1(x)
-        x = self.batchnorm5(x)
-        x = self.relu4(x)
-        
-        x = self.fc2(x)
-        x = self.batchnorm6(x)
-        x = self.relu4(x)
-        
-        x = self.fc3(x)
-        x = self.relu4(x)
-        
-        x = self.fc4(x)
-        x = self.relu4(x)
-        
-        x=self.fc5(x)
-      
-   
-        x = self.sigmoid(x)
 
-      
-        return x
+        out = self.layer1(x)
+        out = self.layer2(out)
+        out = self.layer3(out)
+        out = self.layer4(out)
+        out = self.layer5(out)
+        out = self.layer6(out)
+        out = self.layer7(out)
+        out = self.layer8(out)
+        out = self.layer9(out)
+        out = self.layer10(out)
+        out = self.layer11(out)
+        out = self.layer12(out)
+        out = self.layer13(out)
+        out = out.view(out.size(0), -1)
+        out = self.fc(out)
+        out = self.fc1(out)
+        out = self.fc2(out)
+        return out
+
